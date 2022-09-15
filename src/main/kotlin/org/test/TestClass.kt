3a32701e4e76fc24f2807aca362778367fffbaff
Mateusz Zawadzki <mat.zawadzki123@gmail.com>
2022-09-15 20:04:57 +0200

    fix

--- src/main/kotlin/org/test/TestClass.kt
+++ src/main/kotlin/org/test/TestClass.kt
@@ -5,13 +5,17 @@ import java.util.concurrent.TimeUnit
 
 @State(Scope.Thread)
 @BenchmarkMode(Mode.All)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
 open class TestClass {
 
-    @JvmStatic
-    fun main(args : Array<String>)
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("hello ")
+        }
+    }
 
-        @Benchmark
+
+    @Benchmark
         @Threads(1)
         @Measurement(time = 1, timeUnit = TimeUnit.SECONDS, iterations = 1)
         fun matrixOneThreads(params: Params) {
